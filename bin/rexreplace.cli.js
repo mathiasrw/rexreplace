4755db709e65a3cb58bc9585ea40b1dfcb154be5
Mathias Wulff <m@rawu.dk>
2020-07-08 06:20:09 +0200

    Update deps

--- bin/rexreplace.cli.js
+++ bin/rexreplace.cli.js
@@ -72,7 +72,7 @@
     var path = require('path');
     var globs = require('globs');
     var now = new Date();
-    var version = '6.0.0';
+    var version = '6.0.1';
     function engine(config) {
         if ( config === void 0 ) config = { engine: 'V8' };
 
