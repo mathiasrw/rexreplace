4eb311b749948ad7b837a2030abd8ea495b5c442
M. Wulff <m@rawu.dk>
2023-07-27 16:42:41 +1000

    release v7.1.2

--- bin/rexreplace.cli.js
+++ bin/rexreplace.cli.js
@@ -72,7 +72,7 @@
     var path = require('path');
     var globs = require('globs');
     var now = new Date();
-    var version = '7.1.1';
+    var version = '7.1.2';
     function engine(config) {
         if ( config === void 0 ) config = { engine: 'V8' };
 
